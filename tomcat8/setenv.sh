18bf23c82e63fc5f46359e37278f586c6d8bcd74
Ahmed Choukri <ahmedchoukri@gmail.com>
2018-06-14 12:51:23 -0400

    Update setenv.sh

--- tomcat8/setenv.sh
+++ tomcat8/setenv.sh
@@ -2,5 +2,5 @@
 export CATALINA_HOME=/opt/AppServer/tomcat
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 export PATH=${JAVA_HOME}/bin:${CATALINA_HOME}:${PATH}
-export JAVA_OPTS="$JAVA_OPTS -Xms1024M -Xmx2048M"
+export JAVA_OPTS="$JAVA_OPTS -Xms2048 -Xmx2048M"
 export CATALINA_PID=${CATALINA_HOME}/catalina.pid
